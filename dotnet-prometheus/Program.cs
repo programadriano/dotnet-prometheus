d5c52b23c205dc1bc84436cbc3aa1ca0f834c5da
Thiago da Silva Adriano <thiago.adriano26@gmail.com>
2023-02-18 21:43:31 -0300

    Add files via upload

---
+++ dotnet-prometheus/Program.cs
@@ -0,0 +1,45 @@
+using Prometheus;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+/*INICIO DA CONFIGURAÇÃO - PROMETHEUS*/
+// Custom Metrics to count requests for each endpoint and the method
+var counter = Metrics.CreateCounter("webapimetric", "Counts requests to the WebApiMetrics API endpoints",
+    new CounterConfiguration
+    {
+        LabelNames = new[] { "method", "endpoint" }
+    });
+
+app.Use((context, next) =>
+{
+    counter.WithLabels(context.Request.Method, context.Request.Path).Inc();
+    return next();
+});
+
+// Use the prometheus middleware
+app.UseMetricServer();
+app.UseHttpMetrics();
+
+/*FIM DA CONFIGURAÇÃO - PROMETHEUS*/
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
